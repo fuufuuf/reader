d61f16e9cbce6231aaead45b4d4b8bb1f29b72bc
Tim <timnew.wti@gmail.com>
2018-12-08 16:55:37 +1100

    Implement Book View

--- lib/ng/presentations/BookView.dart
+++ lib/ng/presentations/BookView.dart
@@ -1,5 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:reader/ng/events/EventBus.dart';
+import 'package:reader/ng/events/OpenUrlEvent.dart';
 import 'package:reader/ng/models/Book.dart';
+import 'package:reader/ng/models/ReadingTheme.dart';
+import 'package:reader/ng/presentations/ReadingThemeProvider.dart';
 
 class BookView extends StatelessWidget {
   final Book book;
@@ -7,8 +12,51 @@ class BookView extends StatelessWidget {
   BookView({this.book});
 
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+  Widget build(BuildContext context) =>
+      _renderWithTheme(
+          context, ReadingThemeProvider.fetchReadingTheme(context));
+
+  Widget _renderWithTheme(BuildContext context, ReadingTheme theme) =>
+      Scaffold(
+          appBar: AppBar(
+            title: Text(book.title),
+          ),
+          body: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+              child: Column(
+                children: List.of(buttons(), growable: false),
+              )
+          )
+      );
+
+  Iterable<Widget> buttons() sync* {
+    yield Text("作者： ${book.author}");
+
+    if (book.hasMenu) {
+      yield RaisedButton(
+          child: const Text("Menu"),
+          onPressed: () {
+            EventBus.post(OpenUrlEvent(book.menuUrl));
+          }
+      );
+    }
+
+    if (book.hasFirstChapter) {
+      yield RaisedButton(
+          child: const Text("First Page"),
+          onPressed: () {
+            EventBus.post(OpenUrlEvent(book.firstChapterUrl));
+          }
+      );
+    }
+
+    if (book.hasLatestChapter) {
+      yield RaisedButton(
+          child: const Text("Latest Page"),
+          onPressed: () {
+            EventBus.post(OpenUrlEvent(book.latestChapterUrl));
+          }
+      );
+    }
   }
 }
