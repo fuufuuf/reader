a45811f0c6c95a9b8788294c8701caee68bb7607
Tim <timnew.wti@gmail.com>
2018-12-14 20:48:05 +1100

    Get rid of main screen

--- lib/presentations/ReaderApp.dart
+++ lib/presentations/ReaderApp.dart
@@ -1,16 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:reader/presentations/MainScreen.dart';
+import 'package:reader/models/ReadingTheme.dart';
+import 'package:reader/presentations/EmptyContentView.dart';
+import 'package:reader/presentations/ReadingThemeProvider.dart';
 
-class ReaderApp extends StatelessWidget {
+class ReaderApp extends StatefulWidget {
   // This widget is the root of your application.
+
+  @override
+  State<StatefulWidget> createState() =>
+      _ReaderAppState(ReadingTheme.defaultTheme);
+}
+
+class _ReaderAppState extends State<ReaderApp> {
+  ReadingTheme currentTheme;
+
+  _ReaderAppState(this.currentTheme);
+
+  void updateTheme(bool nightMode) {
+    this.setState(() {
+      this.currentTheme = nightMode ?
+      ReadingTheme.darkTheme :
+      ReadingTheme.defaultTheme;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Reader',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MainScreen(),
+        title: 'Reader',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: ReadingThemeProvider(
+            theme: currentTheme,
+            switchTheme: updateTheme,
+            child: EmptyContentView()
+        )
     );
   }
 }
+
+
