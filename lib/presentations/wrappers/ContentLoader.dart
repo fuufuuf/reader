874695ad6527211e5ea1c71152edf9cb0dbcca44
Tim <timnew.wti@gmail.com>
2018-12-17 03:30:15 +1100

    Enhance Content Loader

--- lib/presentations/wrappers/ContentLoader.dart
+++ lib/presentations/wrappers/ContentLoader.dart
@@ -4,26 +4,41 @@ import 'package:reader/presentations/screens/LoadingScreen.dart';
 
 class ContentLoader<T> extends StatelessWidget {
   final Future<T> future;
+  final T initialData;
   final ContentRender<T> render;
+  final bool respondToReloading;
 
-  ContentLoader({Key key, this.future, this.render}) : super(key: key);
+  ContentLoader({
+    Key key,
+    this.future,
+    this.initialData,
+    @required this.render,
+    this.respondToReloading = false
+  }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => FutureBuilder<T>(
+  Widget build(BuildContext context) =>
+      FutureBuilder<T>(
         future: future,
+        initialData: initialData,
         builder: renderChild,
       );
 
   Widget renderChild(BuildContext context, AsyncSnapshot<T> snapshot) {
+    if (respondToReloading &&
+        snapshot.connectionState == ConnectionState.waiting) {
+      return LoadingView();
+    }
+
     if (snapshot.hasData) {
       return render(context, snapshot.data);
     }
 
     if (snapshot.hasError) {
-      return ErrorScreen(error: snapshot.error);
+      return ErrorView(error: snapshot.error);
     }
 
-    return LoadingScreen();
+    return LoadingView();
   }
 }
 
