e8fab5746486ea9e819feb2acc35f2754bfe42a8
Tim <timnew.wti@gmail.com>
2019-12-20 08:18:57 +1100

    Introduce AppIncons

---
+++ lib/application/AppIcons.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/widgets.dart';
+
+class AppIcons {
+  static const IconData bookInfo =
+      IconData(0xe88f, fontFamily: 'MaterialIcons');
+  static const IconData bookId = IconData(0xe0da, fontFamily: 'MaterialIcons');
+  static const IconData bookTitle =
+      IconData(0xe264, fontFamily: 'MaterialIcons');
+  static const IconData bookChapterList =
+      IconData(0xe896, fontFamily: 'MaterialIcons', matchTextDirection: true);
+  static const IconData bookChapter =
+      IconData(0xe867, fontFamily: 'MaterialIcons');
+  static const IconData bookGenre =
+      IconData(0xe574, fontFamily: 'MaterialIcons');
+  static const IconData bookAuthor =
+      IconData(0xe7fd, fontFamily: 'MaterialIcons');
+  static const IconData bookCompleteness =
+      IconData(0xe619, fontFamily: 'MaterialIcons');
+  static const IconData bookUpdateTime =
+      IconData(0xe192, fontFamily: 'MaterialIcons');
+  static const IconData bookTotalCharacters =
+      IconData(0xe8e2, fontFamily: 'MaterialIcons');
+  static const IconData daylightMode =
+      IconData(0xe430, fontFamily: 'MaterialIcons');
+}
