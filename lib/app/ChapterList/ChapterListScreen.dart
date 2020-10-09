78d2ddef862c7e7eab0b473c3ab2e8e2e418dae3
Tim <timnew.wti@gmail.com>
2020-10-10 02:20:47 +1100

    Move Chapter List to new arch

--- lib/app/ChapterList/ChapterListScreen.dart
+++ lib/app/ChapterList/ChapterListScreen.dart
@@ -1,31 +1,21 @@
+import 'package:built_collection/built_collection.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:timnew_reader/app/ChapterList/ChapterListRequest.dart';
 import 'package:timnew_reader/app/routing/AppRouter.dart';
+import 'package:timnew_reader/arch/RenderMixin.dart';
 import 'package:timnew_reader/models/BookIndex.dart';
-import 'package:timnew_reader/models/ChapterList.dart';
 import 'package:timnew_reader/models/ChapterRef.dart';
 import 'package:timnew_reader/presentations/components/ScreenScaffold.dart';
-import 'package:timnew_reader/presentations/wrappers/ContentLoader.dart';
 
-class ChapterListScreen extends StatelessWidget {
-  final BookIndex bookIndex;
+class ChapterListScreen extends StatefulWidget {
+  final ChapterListRequest request;
 
-  ChapterListScreen(this.bookIndex);
+  ChapterListScreen(BookIndex bookIndex)
+      : assert(bookIndex != null),
+        request = ChapterListRequest(bookIndex);
 
   @override
-  Widget build(BuildContext context) => ScreenScaffold(
-        title: bookIndex.bookName,
-        appBarActions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.info_outline),
-            onPressed: () => AppRouter.of(context).openBookInfo(bookIndex),
-          ),
-        ],
-        body: ContentLoader<ChapterList>(
-            future: bookIndex.fetchChapterList(),
-            render: (BuildContext context, ChapterList chapterList) =>
-                ChapterListView(bookIndex: bookIndex, chapterList: chapterList)),
-      );
+  _ChapterListScreenState createState() => _ChapterListScreenState();
 
   static MaterialPageRoute buildRoute(BookIndex bookIndex) => MaterialPageRoute(
         settings: RouteSettings(name: "ChapterList", arguments: bookIndex),
@@ -33,22 +23,64 @@ class ChapterListScreen extends StatelessWidget {
       );
 }
 
-class ChapterListView extends StatelessWidget {
-  final BookIndex bookIndex;
-  final ChapterList chapterList;
+class _ChapterListScreenState extends State<ChapterListScreen> with RenderAsyncSnapshot<BuiltList<ChapterRef>> {
+  ChapterListRequest get request => widget.request;
 
-  ChapterListView({@required this.bookIndex, @required this.chapterList});
+  @override
+  Widget build(BuildContext context) => ScreenScaffold(
+        title: request.bookName,
+        appBarActions: <Widget>[
+          IconButton(icon: Icon(Icons.info_outline), onPressed: _openBookInfo),
+          IconButton(icon: Icon(Icons.refresh), onPressed: _reload),
+          IconButton(icon: Icon(Icons.bookmark_border), onPressed: _scrollToCurrent)
+        ],
+        body: buildStream(request.valueStream),
+      );
+
+  @override
+  Widget buildData(BuildContext context, BuiltList<ChapterRef> data) {
+    return _ChapterListView(request, data);
+  }
+
+  void _openBookInfo() {
+    AppRouter.of(context).openBookInfo(request.bookIndex);
+  }
+
+  void _reload() {
+    request.reload();
+  }
+
+  void _scrollToCurrent() {}
+}
+
+class _ChapterListView extends StatelessWidget {
+  final ChapterListRequest request;
+  final BuiltList<ChapterRef> chapters;
+
+  _ChapterListView(this.request, this.chapters);
 
   @override
   Widget build(BuildContext context) => ListView.builder(
-      itemBuilder: (context, index) => _renderChapterIndex(context, chapterList.chapters[index]),
-      itemCount: chapterList.chapters.length);
-
-  Widget _renderChapterIndex(BuildContext context, ChapterRef index) => ListTile(
-        leading: Icon(Icons.bookmark_border),
-        title: Text(index.title),
-        onTap: () async {
-          await AppRouter.of(context).openReader(bookIndex, index);
-        },
+        itemBuilder: (context, index) => _ChapterEntry(request, index, chapters[index]),
+        itemCount: chapters.length,
       );
+}
+
+class _ChapterEntry extends StatelessWidget {
+  final ChapterListRequest request;
+  final ChapterRef chapter;
+  final int index;
+
+  _ChapterEntry(this.request, this.index, this.chapter);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(Icons.bookmark_border),
+      title: Text(chapter.title),
+      onTap: () async {
+        await AppRouter.of(context).openReader(request.bookIndex, chapter);
+      },
+    );
+  }
 }
