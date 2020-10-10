0c14311abd94faf4a6c5c895946e9d10cbe68c36
Tim <timnew.wti@gmail.com>
2020-10-10 15:35:29 +1100

    Update BookInfoScreen to new arch

---
+++ lib/app/BookInfo/BookInfoRequest.dart
@@ -0,0 +1,18 @@
+import 'dart:async';
+
+import 'package:timnew_reader/arch/Request.dart';
+import 'package:timnew_reader/models/BookIndex.dart';
+import 'package:timnew_reader/models/BookInfo.dart';
+
+class BookInfoRequest extends Request<BookInfo> {
+  final BookIndex bookIndex;
+
+  BookInfoRequest(this.bookIndex);
+
+  String get bookName => bookIndex.bookName;
+
+  @override
+  Future<BookInfo> execute() async {
+    return bookIndex.fetchBookInfo();
+  }
+}
--- lib/app/BookInfo/BookInfoScreen.dart
+++ lib/app/BookInfo/BookInfoScreen.dart
@@ -1,54 +1,80 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:timnew_reader/app/BookInfo/BookInfoRequest.dart';
+import 'package:timnew_reader/arch/RenderMixin.dart';
 import 'package:timnew_reader/models/BookIndex.dart';
 import 'package:timnew_reader/models/BookInfo.dart';
 import 'package:timnew_reader/presentations/components/ScreenScaffold.dart';
-import 'package:timnew_reader/presentations/wrappers/ContentLoader.dart';
 
-class BookInfoScreen extends StatelessWidget {
-  final BookIndex bookIndex;
+class BookInfoScreen extends StatelessWidget with RenderAsyncSnapshot<BookInfo> {
+  final BookInfoRequest request;
 
-  BookInfoScreen({Key key, String bookId})
-      : bookIndex = BookIndex.load(bookId),
-        super(key: key);
+  BookInfoScreen(BookIndex bookIndex)
+      : assert(bookIndex != null),
+        request = BookInfoRequest(bookIndex),
+        super();
 
   @override
   Widget build(BuildContext context) => ScreenScaffold(
-        title: bookIndex.bookName,
-        body: ContentLoader<BookInfo>(
-            future: bookIndex.fetchBookInfo(),
-            render: (BuildContext context, BookInfo bookInfo) => BookInfoView(bookInfo: bookInfo)),
+        title: request.bookName,
+        body: buildStream(request.valueStream),
       );
+
+  @override
+  Widget buildData(BuildContext context, BookInfo bookInfo) {
+    return _BookInfoView(bookInfo);
+  }
 }
 
-class BookInfoView extends StatelessWidget {
+class _BookInfoView extends StatelessWidget {
   final BookInfo bookInfo;
 
-  BookInfoView({this.bookInfo});
+  _BookInfoView(this.bookInfo);
 
   Iterable<Widget> _buildItems(BuildContext context) sync* {
     if (bookInfo.hasAuthor) {
-      yield ListTile(key: Key('author'), leading: Icon(Icons.person), title: Text('作者： ${bookInfo.author}'));
+      yield ListTile(
+        key: Key('author'),
+        leading: Icon(Icons.person),
+        title: Text('作者： ${bookInfo.author}'),
+      );
     }
 
     if (bookInfo.hasGenre) {
-      yield ListTile(key: Key('genre'), leading: Icon(Icons.category), title: Text('类型： ${bookInfo.genre}'));
+      yield ListTile(
+        key: Key('genre'),
+        leading: Icon(Icons.category),
+        title: Text('类型： ${bookInfo.genre}'),
+      );
     }
 
     if (bookInfo.hasCompleteness) {
-      yield ListTile(key: Key('completeness'), leading: Icon(Icons.more), title: Text('状态： ${bookInfo.completeness}'));
+      yield ListTile(
+        key: Key('completeness'),
+        leading: Icon(Icons.more),
+        title: Text('状态： ${bookInfo.completeness}'),
+      );
     }
 
     if (bookInfo.hasLastUpdated) {
       yield ListTile(
-          key: Key('lastUpdated'), leading: Icon(Icons.access_time), title: Text('最近更新时间： ${bookInfo.lastUpdated}'));
+        key: Key('lastUpdated'),
+        leading: Icon(Icons.access_time),
+        title: Text('最近更新时间： ${bookInfo.lastUpdated}'),
+      );
     }
 
     if (bookInfo.hasLength) {
-      yield ListTile(key: Key('length'), leading: Icon(Icons.translate), title: Text('总字数： ${bookInfo.lastUpdated}'));
+      yield ListTile(
+        key: Key('length'),
+        leading: Icon(Icons.translate),
+        title: Text('总字数： ${bookInfo.lastUpdated}'),
+      );
     }
   }
 
   @override
-  Widget build(BuildContext context) => ListView(children: _buildItems(context).toList(growable: false));
+  Widget build(BuildContext context) => ListView(
+        children: _buildItems(context).toList(growable: false),
+      );
 }
