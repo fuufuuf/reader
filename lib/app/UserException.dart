acd55641ea9515b4b416c2b0f609d3ce41657b59
Tim <timnew.wti@gmail.com>
2020-10-10 20:45:19 +1100

    Better Error reporting

--- lib/app/UserException.dart
+++ lib/app/UserException.dart
@@ -10,13 +10,37 @@ class UserException implements Exception {
     return "錯誤: $message";
   }
 
-  void showAsSnackBar(BuildContext context, {MessageType type: MessageType.Error}) {
-    Scaffold.of(context).showSnackBar(SnackBar(content: Message(message, type: type)));
+  ScaffoldFeatureController<SnackBar, SnackBarClosedReason> showAsSnackBar(
+    BuildContext context, {
+    MessageType type: MessageType.Error,
+  }) {
+    return Scaffold.of(context).showSnackBar(
+      SnackBar(
+        content: Message(message, type: type),
+      ),
+    );
+  }
+
+  static Message buildMessageFromError(Object error, {MessageType type: MessageType.Error}) {
+    final message = error is UserException ? error.message : error.toString();
+    return Message(message, type: type);
   }
 
   static Widget renderError(BuildContext context, Object error) {
-    final message = error is UserException ? error.message : error.toString();
+    return Center(
+      child: buildMessageFromError(error),
+    );
+  }
 
-    return Center(child: Message(message));
+  static ScaffoldFeatureController<SnackBar, SnackBarClosedReason> showErrorAsSnackBar(
+    BuildContext context,
+    Object error, {
+    MessageType type: MessageType.Error,
+  }) {
+    return Scaffold.of(context).showSnackBar(
+      SnackBar(
+        content: buildMessageFromError(error, type: type),
+      ),
+    );
   }
 }
